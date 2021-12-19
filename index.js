4b812c5349e909ea38317b16c71472d5574cbd7b
Ilan Santos <ilansalvador@hotmail.com>
2021-12-19 20:03:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// MIDDLEWARES
+app.get("/", (req, res) => {
+    res.status(200).json({message: "OK"})
+})
+
+
 
 const PORT = 8080
 app.listen(8080, () => {
